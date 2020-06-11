08e340b5a99b6d867789f9d29ef96eee4ece22f2
sara-desing <61196540+sara-desing@users.noreply.github.com>
2020-06-11 16:49:49 -0400

    First Draw Scene
    Oop x2

---
+++ drawScene1.js
@@ -0,0 +1,20 @@
+//Code for the first scene
+function drawScene1() {
+  sceneNum = 1;
+  background(135, 206, 235);
+  fill(176, 196, 222)
+  rect(40, -2, 55, 190);
+  rect(210, -2, 55, 100);
+  rect(380, -2, 55, 135);
+  
+  rect(40, 350, 55, 280);
+  rect(210, 280, 55, 480);
+  rect(380, 370, 55, 480);
+  
+  fill(216,191, 216)
+  rect(290, 150, 40, 40);
+  
+  fill(105,105,105);
+  textSize(30);
+  text("Welcome to Blocky Bird!", 80, 240);
+}
